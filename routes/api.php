50d688d524c4ab3fed531fde76e27ba717d78e68
Your Name <you@example.com>
2025-02-19 22:06:17 +0800

    api routes

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return response()->json([
+        'name' => 'John Doe',
+        'email' => 'john@example.com',
+    ]);
+});
